81c605ee9b6a873199184c8fe06d682508b21318
k8abadzheva <make.me.loco@gmail.com>
2025-02-20 17:34:51 +0300

    Add runtime permission check for contacts
    - Check if the `READ_CONTACTS` permission is granted before requesting contacts.
    - Log a message if the permission is not granted.
    - Retrieve the contact ID and name, then log each contact.

--- app/src/main/java/com/abadzheva/readcontacts/MainActivity.kt
+++ app/src/main/java/com/abadzheva/readcontacts/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.abadzheva.readcontacts
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.ContactsContract.Contacts
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.concurrent.thread
@@ -13,13 +17,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) {
+            v,
+            insets,
+            ->
+            val systemBars =
+                insets.getInsets(
+                    WindowInsetsCompat
+                        .Type
+                        .systemBars(),
+                )
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom,
+            )
             insets
         }
 
-        requestContacts()
+        // -----------------------------------------------
+        val permissionGranted =
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_CONTACTS,
+            ) == PackageManager.PERMISSION_GRANTED
+        if (permissionGranted) {
+            requestContacts()
+        } else {
+            Log.d("Contacts", "Permission denied")
+        }
     }
 
     private fun requestContacts() {
@@ -32,6 +59,22 @@ class MainActivity : AppCompatActivity() {
                     null,
                     null,
                 )
+            while (cursor?.moveToNext() == true) {
+                val id =
+                    cursor.getInt(
+                        cursor.getColumnIndexOrThrow(
+                            Contacts._ID,
+                        ),
+                    )
+                val name =
+                    cursor.getString(
+                        cursor.getColumnIndexOrThrow(
+                            Contacts.DISPLAY_NAME,
+                        ),
+                    )
+                val contact = Contact(id, name)
+                Log.d("Contacts", contact.toString())
+            }
             cursor?.close()
         }
     }
